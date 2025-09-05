6fb4f6d6d4e5f0eeb50b7b94f5166e1649c5c348
Mohit Saha Chowdhdhury <sahachowdhurymohit@gmail.com>
2025-09-06 01:21:17 +0530

    fix: Added cors for deployment

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from app.routes import auth_routes, note_routes
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Notes App API")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 # include routers
 app.include_router(auth_routes.router)
